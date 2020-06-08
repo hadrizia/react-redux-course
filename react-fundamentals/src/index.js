7a3c4a0623b033006c33f83452a603736dd1af35
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-06-08 20:42:35 -0300

    Creating my first react app and index from scratch

---
+++ react-fundamentals/src/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+const el = <strong> Hello, World! </strong>
+
+ReactDOM.render(
+<div> { el } </div>,  
+  document.getElementById('root')
+)
