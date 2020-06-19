fcd2a67f555b4abc53605278bd53f3646c64a5f2
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-06-18 21:45:01 -0300

    Creating a react fragment component

---
+++ react-fundamentals/src/components/basics/FragmentComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function() {
+  return(
+    <React.Fragment>
+      <p>This is an example of a react fragment.</p>
+      <p>Yay :D</p>
+    </React.Fragment>
+  )
+}
--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -4,20 +4,20 @@ import React from 'react'
 
 import FirstComponent from './components/basics/FirstComponent'
 import ComponentWithProps from './components/basics/ComponentWithProps'
+import FragmentComponent from './components/basics/FragmentComponent'
 
-ReactDOM.render(
-  <div> 
-    <FirstComponent></FirstComponent>
-    <ComponentWithProps
-      name="Maju"
-      animal="cat"
-      >
-    </ComponentWithProps>
-    <ComponentWithProps
-      name="Simbad"
-      animal="dog"
-      >
-    </ComponentWithProps>
-  </div>,  
-  document.getElementById('root')
+ReactDOM.render( 
+    <div>
+        <FirstComponent></FirstComponent>
+        <ComponentWithProps 
+            name = "Maju"
+            animal = "cat" >
+        </ComponentWithProps> 
+        <ComponentWithProps 
+            name = "Simbad"
+            animal = "dog" >
+        </ComponentWithProps> 
+        <FragmentComponent> </FragmentComponent> 
+    </div>,
+    document.getElementById('root')
 )
