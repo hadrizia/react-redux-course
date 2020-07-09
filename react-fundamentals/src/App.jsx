9b039138624c5ad0bfb46f636197457b46c75923
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-07-09 20:43:52 +0100

    Add a new component with children and add some style to cards

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -1,3 +1,5 @@
+import './App.css';
+
 import React from 'react';
 
 import FirstComponent from './components/basics/FirstComponent'
@@ -5,27 +7,38 @@ import ComponentWithProps from './components/basics/ComponentWithProps'
 import FragmentComponent from './components/basics/FragmentComponent'
 import RandomNumber from './components/basics/RandomNumber'
 import Card from './components/layout/Card'
+import Family from './components/basics/Family'
+import FamilyMember from './components/basics/FamilyMember'
 
 export default () => (
   <div>
     <h1>React Fundamentals</h1>
-    <Card titulo="1. First component">
-      <FirstComponent />
-    </Card>
-    <Card titulo="2. Component with props">
-      <ComponentWithProps
-        name="Maju"
-        animal="cat" >
-      </ComponentWithProps>
-    </Card>
-    <Card titulo="3. First React Fragment">
-      <FragmentComponent />
-    </Card>
-    <Card titulo="4. Random Number in a range">
-      <RandomNumber
-        min={1}
-        max={100}>
-      </RandomNumber>
-    </Card>
+    <div className="cards" color="#003F5C">
+      <Card titulo="1. First component">
+        <FirstComponent />
+      </Card>
+      <Card titulo="2. Component with props" color="#58508D">
+        <ComponentWithProps
+          name="Maju"
+          animal="cat" >
+        </ComponentWithProps>
+      </Card>
+      <Card titulo="3. First React Fragment" color="#BC5090">
+        <FragmentComponent />
+      </Card>
+      <Card titulo="4. Random Number from a range" color="#FF6361">
+        <RandomNumber
+          min={1}
+          max={100}>
+        </RandomNumber>
+      </Card>
+      <Card titulo="5. Component with children" color="#FFA600">
+        <Family sobrenome="Santos">
+          <FamilyMember nome="Hadrizia"/>
+          <FamilyMember nome="Marianne"/>
+          <FamilyMember nome="Maju"/>
+        </Family>
+      </Card>
+    </div>
   </div>
 );
--- react-fundamentals/src/components/layout/Card.jsx
+++ react-fundamentals/src/components/layout/Card.jsx
@@ -3,8 +3,13 @@ import './Card.css'
 import React from 'react'
 
 export default (props) => {
+  const cardStyle = {
+    backgroundColor: props.color || "#6f6699",
+    borderColor: props.color || "#6f6699"
+  }
+
   return (
-    <div className="card">
+    <div className="card" style={cardStyle}>
       <div className="card-title"><h3>{props.titulo}</h3></div>
       <div className="card-content"> {props.children} </div>
     </div>
